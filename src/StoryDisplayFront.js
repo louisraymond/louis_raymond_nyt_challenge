ae0e293a64f43c5bd41f02113c602222b9ef0da9
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2019-01-30 16:31:58 +0100

    got stories to render required information

--- src/StoryDisplayFront.js
+++ src/StoryDisplayFront.js
@@ -2,13 +2,42 @@ import React from 'react';
 
 class StoryDisplayFront extends React.Component {
 
-render () {
-  return (
-    <div>
-      <h2>{this.props.story.title}</h2>
-      <img src={this.props.story.thumbnail_standard}/>
+state = {
+  isClicked : false
+}
 
-    </div>)}
+handleClick = () => {
+  this.setState({isClicked:!this.state.isClicked})
+}
+
+TitleOrInfo = (isClicked) => {
+  if (isClicked === true){
+    return (
+      <div onClick={this.handleClick}>
+        <p>{this.props.story.abstract}</p>
+        <a href={this.props.story.url}>Click Here To Read The Story!</a>
+
+      </div>
+    )
+
+  }else{
+    return (
+      <div onClick={this.handleClick}>
+        <h2>{this.props.story.title}</h2>
+        <img src={this.props.story.thumbnail_standard}/>
+
+      </div>)
+  }
+
+}
+
+render () {
+    return(
+      <div>
+      {this.TitleOrInfo(this.state.isClicked)}
+      </div>
+    )
+  }
 
 }
 
