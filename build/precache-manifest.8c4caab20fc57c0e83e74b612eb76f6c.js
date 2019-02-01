018e3b13807a55b629626e1b3e279a78ba786673
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2019-02-01 15:33:24 +0100

    added a build version

---
+++ build/precache-manifest.8c4caab20fc57c0e83e74b612eb76f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d73255cda1b7db676b21",
+    "url": "/static/css/main.916f7188.chunk.css"
+  },
+  {
+    "revision": "d73255cda1b7db676b21",
+    "url": "/static/js/main.d73255cd.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00c0f62da90c7a456120e8e4643dcca5",
+    "url": "/index.html"
+  }
+];
